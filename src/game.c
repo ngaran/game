48baa02f9ebdf0c75dd364a6ab7db3be1bad56ea
ngaran94 <ngaran94@gmail.com>
2022-06-02 18:45:26 -0500

    get player name

--- src/game.c
+++ src/game.c
@@ -1,5 +1,43 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define NAME_SIZE 255
+char *name;
+
+const char *name_p = "what will you call yourself";
+// const char *hello = "hello %s\n"; unused
+
+int my, mx;
+
 int main(int argc, char **argv)
 {
+	name = malloc(NAME_SIZE);
+
+	initscr();
+
+	getmaxyx(stdscr, my, mx);
+
+	mvprintw(4, (int)mx/2-strlen(name_p)/2, "%s\n", name_p);
+
+	mvgetstr(5, (int)mx/2, name);
+
+	curs_set(0);
+
+	/*
+	 *  clear();
+	 *  mvprintw(4, (int)mx/2, hello, name);
+	 *
+	 *  unused
+	 */
+
+	getch();
+
+	endwin();
+
+	free(name);
+
 	return 0;
 }
 
